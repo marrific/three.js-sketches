a4752c9aee1ed39fb5f28b69cb0eca1b18152baa
marrific <marcela.nowak@nyu.edu>
2016-03-04 22:19:22 -0500

    three.js

---
+++ animated texture/assets/geo.js
@@ -0,0 +1,12 @@
+{
+	"metadata": {
+		"version": 4.4,
+		"type": "Geometry",
+		"generator": "Geometry.toJSON"
+	},
+	"uuid": "016AD58B-36C0-4B2A-B9B3-0D31ED34BC9E",
+	"type": "BoxGeometry",
+	"width": 1,
+	"height": 1,
+	"depth": 1
+}
